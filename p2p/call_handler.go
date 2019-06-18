2e8d15af31c1df9563aad9f0331a3c8b36528f15
hwx <838424414@qq.com>
2019-06-18 09:32:19 +0800

    second

---
+++ p2p/call_handler.go
@@ -0,0 +1,27 @@
+package p2p
+
+type P2PServerV2 struct {
+
+}
+
+//注册订阅 注册操作消息 回调函数
+func(p *P2PServerV2 ) registerSubscriber() error{
+
+
+
+	return nil
+}
+
+//  对操作身份权限的回调函数
+func (p *P2PServerV2) handleGetAuthentication() error{
+
+	return nil
+}
+
+
+//
+
+func errorHandleGetAuthenMsg(logid string)  {
+
+}
+
